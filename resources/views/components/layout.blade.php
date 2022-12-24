dfd6bec5d0cfa732f71b315eaa642e0a73d7bddd
wenderl <wender_ieu_lima@hotmail.com>
2022-12-23 22:23:29 -0400

    Initial commit

---
+++ resources/views/components/layout.blade.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>{{ $title }} - Controle de Séries</title>
+    @vite(['resources/sass/app.scss'])
+</head>
+<body>
+<div class="container">
+    <h1>{{ $title }}</h1>
+
+    {{ $slot }}
+</div>
+@vite(['resources/js/app.js'])
+</body>
+</html>
