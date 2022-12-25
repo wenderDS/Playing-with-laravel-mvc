73a9fb690f1149fbbadbc10261a930c3896832fd
wenderl <wender_ieu_lima@hotmail.com>
2022-12-25 15:35:25 -0400

    refactor of language

--- resources/views/series/create.blade.php
+++ resources/views/series/create.blade.php
@@ -1,11 +1,11 @@
-<x-layout title="Nova Série">
-    <form action="/series/salvar" method="post">
+<x-layout title="New series">
+    <form action="{{ route('series.store') }}" method="post">
         @csrf
         <div class="mb-3">
-            <label for="nome" class="form-label">Nome:</label>
-            <input type="text" id="nome" name="nome" class="form-control">
+            <label for="name" class="form-label">Name:</label>
+            <input type="text" id="name" name="name" class="form-control">
         </div>
 
-        <button type="submit" class="btn btn-primary">Adicionar</button>
+        <button type="submit" class="btn btn-primary">Save</button>
     </form>
 </x-layout>
