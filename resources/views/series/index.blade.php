79140ab43fc8ba65a643f1cd706eb11b705c7258
wenderl <wender_ieu_lima@hotmail.com>
2022-12-26 14:18:51 -0400

    refactor in the foreach item

--- resources/views/series/index.blade.php
+++ resources/views/series/index.blade.php
@@ -1,18 +1,30 @@
 <x-layout title="Series">
     <a href="{{ route('series.create') }}" class="btn btn-dark mb-2">New</a>
 
-    <ul class="list-group">
-        @foreach ($series as $serie)
-        <li class="list-group-item d-flex justify-content-between align-items-center">
-            {{ $serie->name }}
+    @isset($successMessage)
+    <div class="alert alert-success">
+        {{ $successMessage }}
+    </div>
+    @endisset
 
-            <form action="{{ route('series.destroy', $serie->id) }}" method="post">
-                @csrf
-                @method('DELETE')
-                <button class="btn btn-danger btn-sm">
-                    Remove
-                </button>
-            </form>
+    <ul class="list-group">
+        @foreach ($series as $item)
+        <li class="list-group-item d-flex justify-content-between align-items-center">
+            {{ $item->name }}
+
+            <span class="d-flex">
+                <a href="{{ route('series.edit', $item->id) }}" class="btn btn-info text-white btn-sm me-1">
+                    Edit
+                </a>
+
+                <form action="{{ route('series.destroy', $item->id) }}" method="post">
+                    @csrf
+                    @method('DELETE')
+                    <button class="btn btn-danger btn-sm">
+                        Remove
+                    </button>
+                </form>
+            </span>
         </li>
         @endforeach
     </ul>
