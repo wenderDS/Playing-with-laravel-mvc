00911a1d2ae2976e0142e2705af42df172762c93
wenderl <wender_ieu_lima@hotmail.com>
2022-12-26 23:40:52 -0400

    format code to define the method in the form edit and refactor code

--- resources/views/series/create.blade.php
+++ resources/views/series/create.blade.php
@@ -1,3 +1,7 @@
 <x-layout title="New series">
-    <x-series.form :action="route('series.store')"></x-series.form>
+    <x-series.form
+        :action="route('series.store')"
+        :input-name-value="old('name')"
+        :update="false"
+    />
 </x-layout>
--- resources/views/series/edit.blade.php
+++ resources/views/series/edit.blade.php
@@ -1,3 +1,7 @@
 <x-layout title="Edit series '{{ $series->name  }}'">
-    <x-series.form :action="route('series.update', $series->id)" :input-name-value="$series->name"></x-series.form>
+    <x-series.form
+        :action="route('series.update', $series->id)"
+        :input-name-value="$series->name"
+        :update="true"
+    />
 </x-layout>
