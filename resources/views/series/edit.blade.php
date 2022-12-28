edba4ec149bbef84d2084edfd421682a745d5e74
wenderl <wender_ieu_lima@hotmail.com>
2022-12-28 00:47:33 -0400

    refactor of view create and edit

--- resources/views/series/edit.blade.php
+++ resources/views/series/edit.blade.php
@@ -1,7 +1,19 @@
-<x-layout title="Edit series '{{ $series->name  }}'">
-    <x-series.form
-        :action="route('series.update', $series->id)"
-        :input-name-value="$series->name"
-        :update="true"
-    />
+<x-layout title="Edit series '{!! $series->name !!}'">
+    <form action="{{ route('series.update', $series->id) }}" method="post">
+        @csrf
+        @method('PUT')
+
+        <div class="mb-3">
+            <label for="name" class="form-label">Name:</label>
+            <input
+                type="text"
+                id="name"
+                name="name"
+                class="form-control"
+                value="{{ $series->name }}"
+            />
+        </div>
+
+        <button type="submit" class="btn btn-primary">Save</button>
+    </form>
 </x-layout>
