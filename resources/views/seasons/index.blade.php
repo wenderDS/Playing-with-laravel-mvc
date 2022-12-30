f3846782faf96ab2a938a2fa61676d181435da89
wenderl <wender_ieu_lima@hotmail.com>
2022-12-30 12:46:45 -0400

    count episodes watched

--- resources/views/seasons/index.blade.php
+++ resources/views/seasons/index.blade.php
@@ -4,10 +4,12 @@
     <ul class="list-group">
         @foreach ($seasons as $season)
             <li class="list-group-item d-flex justify-content-between align-items-center">
-                Season {{ $season->number }}
+                <a href="{{ route('episodes.index', $season->id) }}">
+                    Season {{ $season->number }}
+                </a>
 
                 <span class="badge bg-secondary">
-                    {{ $season->episodes->count() }}
+                    {{ $season->numberOfWatchedEpisodes() }} / {{ $season->episodes->count() }}
                 </span>
             </li>
         @endforeach
